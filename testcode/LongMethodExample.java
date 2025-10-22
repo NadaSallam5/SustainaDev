f097eb10c3ffc79cd23e75a83d40bbb322950293
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-10-22 22:27:01 +0300

    Extract Method in processOrder: CCN 4→1

--- testcode/LongMethodExample.java
+++ testcode/LongMethodExample.java
@@ -1,83 +1,27 @@
 public class LongMethodExample {
 
-    public void massiveMethod() {
-        int total = 0;
+    public class TestRefactor {
 
-        for (int i = 0; i < 10; i++) {
-            total += i;
-            extractedHelper();
+        public void processOrder(int[] prices) {
+            int total = 0;
+            int count = 0;
 
-            for (int j = 0; j < 10; j++) {
-                total += j;
-                System.out.println("J = " + j);
+        extractedHelper();
             }
-            extractedHelper();
-            total += e;
-            System.out.println("E = " + e);
+
+            double avg = (count == 0) ? 0 : (double) total / count;
+            System.out.println("Average price: " + avg);
+
+            System.out.println("Order processed successfully.");
         }
 
-        for (int f = 0; f < 10; f++) {
-            total += f;
-            System.out.println("F = " + f);
-        }
+    }}
 
-        for (int g = 0; g < 10; g++) {
-            total += g;
-            System.out.println("G = " + g);
-        }
-
-        System.out.println("Done. Total = " + total);
-    }
-
-}
-
-    private void extractedHelper() {
-                    if (k % 5 == 0) {
-                        System.out.println("Multiple of 5: " + k);
-                    }
-                }
-
-    for(
-
-    int a = 0;a<10;a++)
-    {
-        total += a;
-        System.out.println("A = " + a);
-    }
-
-    for(
-    int b = 0;b<10;b++)
-    {
-    }
-
-    private void extractedHelper() {
-        
-                for (int k = 0; k < 10; k++) {
-                    total += k;
-                    extractedHelper();
-                    total += b;
-                    if (b % 3 == 0) {
-                        System.out.println("Multiple of 3: " + b);
-                    }
-                }
-        
-                for (int c = 0; c < 10; c++) {
-    }
-
-    private void extractedHelper() {
-                    if (i % 2 == 0) {
-                        System.out.println("Even number: " + i);
-                    } else {
-                        System.out.println("Odd number: " + i);
-                    }
-                }
-
-}
-
-private void extractedHelper() {
-        for (int k = 0; k < 10; k++) {
-            if (k % 5 == 0) {
-                System.out.println("Multiple of 5: " + k);
+    private void calculateTotalAndCount(int[] prices, int total, int count) {
+        for (int price : prices) {
+            if (price > 0) {
+                total += price;
+                count++;
             }
         }
-    }
+}
