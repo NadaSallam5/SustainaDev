eb537b23b4cfed0d80e8ca24b68e080fbcfe1612
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-10-25 23:17:23 +0300

    Extract Method in calculateAverage: CCN 3→3

--- testcode/Mysample.java
+++ testcode/Mysample.java
@@ -4,7 +4,7 @@ public class Mysample {
         int total = 0;
         int count = 0;
 
-        int[] results = calculateAverage(prices, total, count);
+        int[] results = calculateTotalAndCount(prices, total, count);
         total = results[0];
         count = results[1];
 
@@ -16,6 +16,16 @@ public class Mysample {
     }
 
     private int[] calculateAverage(int[] prices, int total, int count) {
+        for (int price : prices) {
+            if (price > 0) {
+                total += price;
+                count++;
+            }
+        }
+        return new int[] { total, count };
+    }
+
+    private int[] calculateTotalAndCount(int[] prices, int total, int count) {
         for (int price : prices) {
             if (price > 0) {
                 total += price;
