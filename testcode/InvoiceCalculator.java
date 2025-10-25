811cd667adb4dd7bc06d33cb85224e5b61d16c69
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-10-25 22:55:08 +0300

    Extract Method in processOrder: CCN 3→1

--- testcode/InvoiceCalculator.java
+++ testcode/InvoiceCalculator.java
@@ -4,12 +4,7 @@ public class InvoiceCalculator {
         int total = 0;
         int count = 0;
 
-        for (int price : prices) {
-            if (price > 0) {
-                total += price;
-                count++;
-            }
-        }
+        calculateTotalAndCount(prices, total, count);
 
         double avg = calculateAverage(total, count);
         System.out.println("Order processed successfully.");
@@ -20,4 +15,12 @@ public class InvoiceCalculator {
         return (count == 0) ? 0 : (double) total / count;
     }
 
+    private void calculateTotalAndCount(int[] prices, int total, int count) {
+        for (int price : prices) {
+            if (price > 0) {
+                total += price;
+                count++;
+            }
+        }
+    }
 }
