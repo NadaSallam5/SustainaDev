f4de9b345f93586e16fd47e4f64d1fda95ef2192
Mostafa Yasser Wahba <yassermostafa535@gmail.com>
2025-11-03 03:53:41 +0200

    Extract Method in calculateTotalAndCount

--- testcode/InvoiceCalculator.java
+++ testcode/InvoiceCalculator.java
@@ -49,4 +49,19 @@ public class InvoiceCalculator {
         printAverageAndSuccessMessage(avg);
     }
 
+    private int[] extractAndCalculateTotalAndCount(int[] prices) {
+        int total = 0;
+        int count = 0;
+
+        for (int price : prices) {
+            if (price > 0) {
+                total += price;
+                count++;
+            } else {
+                System.out.println("Invalid price skipped: ");
+            }
+        }
+        return new int[] { total, count };
+    }
+
 }
