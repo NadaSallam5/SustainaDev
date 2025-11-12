f286df18a138386d3ddf66dcb200b3ed203eb692
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-11-13 00:20:29 +0200

    Inline Method in calculateDiscount

--- testcode/DiscountCalculator.java
+++ testcode/DiscountCalculator.java
@@ -1,11 +1,7 @@
 public class DiscountCalculator {
 
-    public static double calculateDiscount(double price, double percentage) {
-        return price - (price * (percentage / 100));
-    }
-
     public static void applyDiscount() {
-        double discountedPrice = calculateDiscount(200, 15);
+        double discountedPrice = (200 - (200 * (15 / 100)));
         System.out.println(discountedPrice);
     }
 }
