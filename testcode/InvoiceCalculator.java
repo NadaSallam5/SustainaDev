f1a84453e0bba694433b781be65bddd7b02dd593
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-11-12 21:26:26 +0200

    Extract Method in processOrder

--- testcode/InvoiceCalculator.java
+++ testcode/InvoiceCalculator.java
@@ -13,7 +13,7 @@ public class InvoiceCalculator {
         int total = 0;
         int count = 0;
 
-        int[] results = processPrices(prices);
+        int[] results = calculateTotalAndCount(prices, total, count);
         total = results[0];
         count = results[1];
 
@@ -28,10 +28,7 @@ public class InvoiceCalculator {
         System.out.println("Number of valid items: " + count);
     }
 
-    private int[] processPrices(int[] prices) {
-        int total = 0;
-        int count = 0;
-
+    private int[] calculateTotalAndCount(int[] prices, int total, int count) {
         for (int price : prices) {
             if (price > 0) {
                 total += price;
