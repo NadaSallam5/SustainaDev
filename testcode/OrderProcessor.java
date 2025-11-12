25b0a243a9ccea7faf32a333f3847f4332057899
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2025-11-12 21:31:03 +0200

    Rename Variable in calculateTotal

--- testcode/OrderProcessor.java
+++ testcode/OrderProcessor.java
@@ -9,9 +9,9 @@ public class OrderProcessor {
 
     public double calculateTotal(double[] prices) {
         double total = 0;
-        for (double p : prices) {
-            if (p > 0) {
-                total += p;
+        for (double itemPrice : prices) {
+            if (itemPrice > 0) {
+                total += itemPrice;
             }
         }
 
